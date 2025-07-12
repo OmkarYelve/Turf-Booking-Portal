9e1ea66c0ddf632130083adce49bffa026318ea8
OmkarYelve <your-email@example.com>
2025-07-12 23:41:19 +0530

    Updated helper and package files

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8080";
